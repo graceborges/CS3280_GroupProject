41a4535f39e4c59cb88ae20654a3f6cc942d7b1d
Grace Borges <graceborges@mail.weber.edu>
2018-11-18 19:22:20 -0700

    My first edit?

--- Add_Mod_Del_Invoice_Project/Add_Mod_Del_Invoice_Project/Search/wndSearch.xaml.cs
+++ Add_Mod_Del_Invoice_Project/Add_Mod_Del_Invoice_Project/Search/wndSearch.xaml.cs
@@ -23,5 +23,25 @@ namespace Add_Mod_Del_Invoice_Project.Search
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// This button will send the selected invoice over to the editor
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This button will search the database for any records matching the info provided
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSearchInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
